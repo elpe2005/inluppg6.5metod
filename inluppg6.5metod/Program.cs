fafa777c5c27d7e4e8acda05b57cb32cdc249cca
Oscar05001 <oskr05001@utb.vaxjo.se>
2023-02-15 11:33:07 +0100

    Add project files.

---
+++ inluppg6.5metod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace inluppg64metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Längst("hej vad heter du", "Vad gör du, eller varför gör du"));
+        }
+        static string Längst(string sträng1, string sträng2)
+        {
+            if (sträng1.Length > sträng2.Length)
+            {
+                return sträng1;
+            }
+            else if (sträng1.Length == sträng2.Length)
+            {
+                return sträng1 + sträng2;
+            }
+            else
+            {
+                return sträng2;
+            }
+        }
+    }
+}
